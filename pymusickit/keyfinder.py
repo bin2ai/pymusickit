850a12505d9978fce328974ae5a6c4c96289120b
chase <tccox3@knights.ucf.edu>
2023-02-19 10:19:16 -0600

    cleaned variable names and methods

--- pymusickit/keyfinder.py
+++ pymusickit/keyfinder.py
@@ -33,11 +33,19 @@ class KeyFinder(object):
     min_key_corrs: list = None  # correlations between pitch class vs minor keys
     maj_key_corrs: list = None  # correlations between pitch class vs major keys
     key_dict: dict = None    # dictionary of the musical keys (major/minor)
-    key: str = None  # key determined by the algorithm
-    bestcorr: float = None  # strength of correlation for the key determined by the algorithm
-    altkey: str = None  # alternative key determined by the algorithm
-    altbestcorr: float = None  # strength of correlation for the alternative key
+    key_primary: str = None  # key determined by the algorithm
+    # strength of correlation for the key determined by the algorithm
+    best_corr_primary: float = None
+    key_alt: str = None  # alternative key determined by the algorithm
+    best_corr_alt: float = None  # strength of correlation for the alternative key
     chroma_max: float = None  # maximum value of the chroma graph
+    pitch_max: str = None  # pitch class with the highest intensity in the song segment
+    pitch_max_corr: float = None  # strength of pitch class with the highest intensity
+    pitches: list = None  # list of pitch classes
+    # major key profile from Krumhansl-Schmuckler algorithm
+    major_profile_ks: list = None
+    # minor key profile from Krumhansl-Schmuckler algorithm
+    minor_profile_ks: list = None
 
     def __init__(self,
                  file: str,  # path to mp3 file
@@ -74,51 +82,70 @@ class KeyFinder(object):
         self.chroma_vals = []
         for i in range(12):
             self.chroma_vals.append(np.sum(self.chromograph[i]))
-        pitches = ['C', 'C#', 'D', 'D#', 'E',
-                   'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
+        self.pitches = ['C', 'C#', 'D', 'D#', 'E',
+                        'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
         # dictionary relating pitch names to the associated intensity in the song
-        self.keyfreqs = {pitches[i]: self.chroma_vals[i] for i in range(12)}
+        self.keyfreqs = {self.pitches[i]
+            : self.chroma_vals[i] for i in range(12)}
 
+        # list of musical keys (major/minor)
         keys = [
-            pitches[i] + ' major' for i in range(12)] + [pitches[i] + ' minor' for i in range(12)]
+            self.pitches[i] + ' major' for i in range(12)] + [self.pitches[i] + ' minor' for i in range(12)]
 
         # use of the Krumhansl-Schmuckler key-finding algorithm, which compares the chroma
         # data above to typical profiles of major and minor keys:
-        maj_profile = [6.35, 2.23, 3.48, 2.33, 4.38,
-                       4.09, 2.52, 5.19, 2.39, 3.66, 2.29, 2.88]
-        min_profile = [6.33, 2.68, 3.52, 5.38, 2.60,
-                       3.53, 2.54, 4.75, 3.98, 2.69, 3.34, 3.17]
+        self.major_profile_ks = [6.35, 2.23, 3.48, 2.33, 4.38,
+                                 4.09, 2.52, 5.19, 2.39, 3.66, 2.29, 2.88]
+        self.minor_profile_ks = [6.33, 2.68, 3.52, 5.38, 2.60,
+                                 3.53, 2.54, 4.75, 3.98, 2.69, 3.34, 3.17]
 
         # finds correlations between the amount of each pitch class in the time interval and the above profiles,
         # starting on each of the 12 pitches. then creates dict of the musical keys (major/minor) to the correlation
         self.min_key_corrs = []
         self.maj_key_corrs = []
         for i in range(12):
-            key_test = [self.keyfreqs.get(pitches[(i + m) % 12])
+            key_test = [self.keyfreqs.get(self.pitches[(i + m) % 12])
                         for m in range(12)]
             # correlation coefficients (strengths of correlation for each key)
             self.maj_key_corrs.append(
-                round(np.corrcoef(maj_profile, key_test)[1, 0], 3))
+                round(np.corrcoef(self.major_profile_ks, key_test)[1, 0], 3))
             self.min_key_corrs.append(
-                round(np.corrcoef(min_profile, key_test)[1, 0], 3))
+                round(np.corrcoef(self.minor_profile_ks, key_test)[1, 0], 3))
 
         # names of all major and minor keys
         self.key_dict = {**{keys[i]: self.maj_key_corrs[i] for i in range(12)},
                          **{keys[i+12]: self.min_key_corrs[i] for i in range(12)}}
 
         # this attribute represents the key determined by the algorithm
-        self.key = max(self.key_dict, key=self.key_dict.get)
-        self.bestcorr = max(self.key_dict.values())
+        self.key_primary = max(self.key_dict, key=self.key_dict.get)
+        self.best_corr_primary = max(self.key_dict.values())
 
         # this attribute represents the second-best key determined by the algorithm,
         # if the correlation is close to that of the actual key determined
-        self.altkey = None
-        self.altbestcorr = None
+        self.key_alt = None
+        self.best_corr_alt = None
 
+        best_corr_alt = 0
         for key, corr in self.key_dict.items():
-            if corr > self.bestcorr*0.9 and corr != self.bestcorr:
-                self.altkey = key
-                self.altbestcorr = corr
+            # if the correlation is close to the best correlation, but not the same,
+            # if corr > self.best_corr_primary*0.9 and corr != self.best_corr_primary:
+            #     self.key_alt = key
+            #     self.best_corr_alt = corr
+            # get the second best key
+            if corr < self.best_corr_primary and corr >= best_corr_alt:
+                self.key_alt = key
+                best_corr_alt = corr
+                self.best_corr_alt = best_corr_alt
+
+        # done initializing
+
+    # get primary key and correlation
+    def get_primary_key_corr(self) -> tuple:
+        return self.key_primary, self.best_corr_primary
+
+    # get secondary key and correlation
+    def get_secondary_key_corr(self) -> tuple:
+        return self.key_alt, self.best_corr_alt
 
     # prints the relative prominence of each pitch class
     def print_chroma(self) -> None:
@@ -127,17 +154,17 @@ class KeyFinder(object):
             print(key, '\t', f'{chrom/self.chroma_max:5.3f}')
 
     # prints the correlation coefficients associated with each major/minor key
-    def corr_table(self) -> None:
+    def print_corr_table(self) -> None:
         for key, corr in self.key_dict.items():
             print(key, '\t', f'{corr:6.3f}')
 
     # printout of the key determined by the algorithm; if another key is close, that key is mentioned
     def print_key(self) -> None:
         print("likely key: ", max(self.key_dict, key=self.key_dict.get),
-              ", correlation: ", self.bestcorr, sep='')
-        if self.altkey is not None:
-            print("also possible: ", self.altkey,
-                  ", correlation: ", self.altbestcorr, sep='')
+              ", correlation: ", self.best_corr_primary, sep='')
+        if self.key_alt is not None:
+            print("also possible: ", self.key_alt,
+                  ", correlation: ", self.best_corr_alt, sep='')
 
     # prints a chromagram of the file, showing the intensity of each pitch class over time
     def show_chromagram(self, title: str = 'Chromagram') -> None:
@@ -161,9 +188,14 @@ if __name__ == '__main__':
     file = 'angry-birds-theme-song.mp3'
     song = KeyFinder(file)
     song.print_chroma()
-    song.corr_table()
+    song.print_corr_table()
     song.print_key()
     song.show_chromagram()
+
+    # print primary key and correlation
+    print(song.get_primary_key_corr())
+    # print secondary key and correlation
+    print(song.get_secondary_key_corr())
 
     # for attr in dir(song):
     #     if not attr.startswith('_'):
--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 from setuptools import setup, find_packages
+import pathlib
+
+# The directory containing this file
+here = pathlib.Path(__file__).parent.resolve()
+
+# Get the long description from the README file
+long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name='pymusickit',
-    version='0.1',
+    version='0.1.1',
     packages=find_packages(),
     install_requires=[
         'librosa==0.9.2',
@@ -15,6 +22,8 @@ setup(
         ],
     },
     description='A Python package for music analysis.  Keyfinder Forked from "https://github.com/jackmcarthur/musical-key-finder"',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='bin2ai',
     url='https://github.com/bin2ai/pymusickit',
     classifiers=[
